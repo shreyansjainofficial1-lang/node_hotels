eb2807fd9d1bcc588a7e0aeadc9a196d5513a64d
Shreyans <shreyans@example.com>
2025-09-26 21:56:56 +0530

    shift mongo to online server

--- db.js
+++ db.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongooseUrl ='mongodb://localhost:27017/hotels';
+// const mongooseUrl ='mongodb://localhost:27017/hotels';
 
+// const mongooseUrl =    'mongodb+srv://shreyansjainofficial1_db_user:0kiJvtL3PDwBhe3s@cluster0.uyuvcah.mongodb.net/'
+// const MONGODB_URL = process.env.MONGODB_URL;
+
+const mongoURL = process.env.MONGODB_URL_LOCAL;
+// const mongoURL =process.env.MONGODB_URL;
 // set up mongo db connection
-mongoose.connect(mongooseUrl,{
+mongoose.connect(mongoURL,{
     usenewUrlParser: true,
     useUnifiedTopology: true
 })
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); 
+
+const PORT = process.env.PORT || 3000;
 
 //Import the router files
 
@@ -23,6 +26,6 @@ app.get('/', function(req,res){
 
 
 
-app.listen(3000, () =>{
+app.listen(PORT, () =>{
     console.log("listening on port 3000")
 })
